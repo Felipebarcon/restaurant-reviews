a6bebd9533de2e1908e86d0d3e07c9527c75b1e7
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-07 17:21:41 +0100

    app ok

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+  # GET /restaurant/:restaurant_id/reviews/new
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  # GET /restaurant/:restaurant_id/reviews
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  # DELETE /reviews/:id
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+    # list several restaurants -> collection
+    collection do
+      get :top   # /restaurant/top
+    end
+    # routes for one restaurant
+    member do
+      get :chef # /restaurant/:id/chef
+    end
+  end
+
+  resources :restaurant do
+    resources :reviews, only: [ :new, :create ]
+  end
+  # /restaurants/new
+  # GET /restaurant/:restaurant_id/reviews/new -> #new
+  # GET /restaurant/:restaurant_id/reviews/new -> # create
+  resources :reviews, only: [ :destroy ]
+  # Shadow Nesting
 end
